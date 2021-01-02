99f6033b9dc3e52d269b784afbca6df3bd78382b
Ianick Noejovich <ianicknoejovich@51909-Ianick-Noejovich.local>
2021-01-02 11:56:08 -0300

    agrego gitignore para no subir archivos ejecutables o binarios

--- Code/Debug/Src/DR_c/DR_GPIO.d
+++
@@ -1,6 +0,0 @@
-Src/DR_c/DR_GPIO.o: ../Src/DR_c/DR_GPIO.c ../Inc/DR_h/DR_GPIO.h \
- ../Inc/System.h
-
-../Inc/DR_h/DR_GPIO.h:
-
-../Inc/System.h:
--- Code/Debug/Src/DR_c/DR_Systick.d
+++
@@ -1,6 +0,0 @@
-Src/DR_c/DR_Systick.o: ../Src/DR_c/DR_Systick.c ../Inc/DR_h/DR_Systick.h \
- ../Inc/System.h
-
-../Inc/DR_h/DR_Systick.h:
-
-../Inc/System.h:
